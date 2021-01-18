12fc50ec123b3752822c4423fd691f35df686932
aayushiBhardwaj <77406110+aayushiBhardwaj@users.noreply.github.com>
2021-01-18 23:05:47 +0530

    Update exec.sh

--- BSTACK_DEMO-main/exec.sh
+++ BSTACK_DEMO-main/exec.sh
@@ -1,6 +1,7 @@
 
 export BROWSERSTACK_USERNAME=aayushibhardwaj1
 export BROWSERSTACK_ACCESS_KEY=DX3RqCMbzeKwjRxpBzTa
+export BROWSERSTACK_BUILD_NAME=build 4.5
 export TEST_ENV=browserstack
 cd src
 python controller.py
